50ecef70dec738086149ba358f9d29202b7099ef
raoof <raoofalaaeldeen@gmail.com>
2026-03-24 14:36:54 +0200

    3ala nadafa

--- failed_log.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#log file to store failed attempts
-Failed_log_file=/home/raoof/GraduationProject/FaceRecognition/virtualenvironment/log/FailedAttempts.log
-
-
-
-echo "Unsuccessful Attempt at "$(date "+%d-%B-%Y  %A     %T") >> $Failed_log_file
